3f858b54ad514c118f7a7f3a251cdaf5cdd9e428
nikkehtine <nikkehtine@int.pl>
2023-08-19 21:37:11 +0200

    Move worldgen stuff to logic

--- goadventure.go
+++ goadventure.go
@@ -6,12 +6,10 @@ import (
 	"github.com/fatih/color"
 
 	"gitlab.com/nikkehtine/go-text-adventure/logic"
-	"gitlab.com/nikkehtine/go-text-adventure/worldgen"
 )
 
 func main() {
 	fmt.Println(logic.Test())
-	fmt.Println(worldgen.Test())
 	yellow := color.New(color.FgYellow).SprintFunc()
 	red := color.New(color.FgRed).SprintFunc()
 	fmt.Printf("This is a %s and this is %s.\n", yellow("warning"), red("error"))
