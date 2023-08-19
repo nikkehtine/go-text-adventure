c75e2234817ab2bdd97eb072b0d3412031d6d821
nikkehtine <nikkehtine@int.pl>
2023-08-19 16:55:39 +0200

    Add 'color' package

--- goadventure.go
+++ goadventure.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"github.com/fatih/color"
+
 	"gitlab.com/nikkehtine/go-text-adventure/logic"
 	"gitlab.com/nikkehtine/go-text-adventure/worldgen"
 )
@@ -10,4 +12,7 @@ import (
 func main() {
 	fmt.Println(logic.Test())
 	fmt.Println(worldgen.Test())
+	yellow := color.New(color.FgYellow).SprintFunc()
+	red := color.New(color.FgRed).SprintFunc()
+	fmt.Printf("This is a %s and this is %s.\n", yellow("warning"), red("error"))
 }
